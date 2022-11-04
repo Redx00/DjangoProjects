2083f0ff259f32818e09560662c884767917a2ed
Olanshile Riliwan Opeyemi <88075951+Redx00@users.noreply.github.com>
2022-11-04 06:12:52 +0000

    v2 inprogress

--- DrinksProject/DrinkApi/views.py
+++ DrinksProject/DrinkApi/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from drinkapp.models import Drink
+from DrinkApi.serializers import DrinkSerializer
+from django.http import JsonResponse
 
 # Create your views here.
+# @api_view(['GET'])
+def drinkList(request):
+
+    # Get all the drinks
+    # Serialize them
+    # Return json
+    drinks = Drink.objects.all()
+    serializer = DrinkSerializer(drinks, many=True)
+    return Response(serializer.data)
